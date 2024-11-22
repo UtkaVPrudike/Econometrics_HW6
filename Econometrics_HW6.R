61a9a465ce5126538496c6c1e140941c621760d9
UtkaVPrudike <d.e.safiullin@gmail.com>
2024-11-23 00:25:51 +0300

    Update Econometrics_HW6.R

--- Econometrics_HW6.R
+++ Econometrics_HW6.R
@@ -52,3 +52,52 @@ linearHypothesis(lm.3.2, myH0, vcov. = vcovHC(lm.3.2, "HC1"))
 
 probit.4.1 <- glm(smoker ~ smkban + female + age + age^2 + hsdrop + hsgrad + colsome + colgrad + black + hispanic, data = data, family = binomial(link = probit))
 summary(probit.4.1)
+
+Mr.A <- list(smkban = 1, female = 0, age = 20, hsdrop = 1, hsgrad = 0, colsome = 0, colgrad = 0, black = 0, hispanic = 0)
+predict(probit.4.1, Mr.A, type = "response")
+
+coeftest(probit.4.1, vcov.=vcovHC(probit.4.1, "HC1"))[2,]
+# The t value is -5.2 for a null hypothesis that there is no difference in the probability of smoking between workers affected by a smoke ban and workers not affected by such a ban for probit.4.1
+# Thus, reject the null hypothesis at 5% significance
+# The results for probit.4.1 and lm.3.2 are very similar, with t values differing only by around 2%
+
+linearHypothesis(probit.4.1, myH0, vcov. = vcovHC(probit.4.1, "HC1"))
+# The null hypothesis that coefficients on education dummies are all zero is rejected at 5% significance, just like for lm.3.2
+
+Mr.A_no_ban <- list(smkban = 0, female = 0, age = 20, hsdrop = 1, hsgrad = 0, colsome = 0, colgrad = 0, black = 0, hispanic = 0)
+Ms.B <- list(smkban = 1, female = 1, age = 40, hsdrop = 0, hsgrad = 0, colsome = 0, colgrad = 1, black = 1, hispanic = 0)
+Ms.B_no_ban <- list(smkban = 0, female = 1, age = 40, hsdrop = 0, hsgrad = 0, colsome = 0, colgrad = 1, black = 1, hispanic = 0)
+rows <- c('Estimated coefficient on smkban', '(standard errors in parentheses)', 'Predicted probabilities of smoking for Mr. A:', '(i) with smoking ban', '(ii) without smoking ban', 'Difference, (i)- (ii)',
+          'Predicted probabilities of smoking for Mr. B:', '(iii) with smoking ban', '(iv) without smoking ban', 'Difference, (iii)- (iv)')
+probit_model <- c(probit.4.1$coefficients[2],
+                  coeftest(probit.4.1, vcov.=vcovHC(probit.4.1, "HC1"))[2,2],
+                  NA,
+                  predict(probit.4.1, Mr.A, type = "response"),
+                  predict(probit.4.1, Mr.A_no_ban, type = "response"),
+                  predict(probit.4.1, Mr.A, type = "response") - predict(probit.4.1, Mr.A_no_ban, type = "response"),
+                  NA,
+                  predict(probit.4.1, Ms.B, type = "response"),
+                  predict(probit.4.1, Ms.B_no_ban, type = "response"),
+                  predict(probit.4.1, Ms.B, type = "response") - predict(probit.4.1, Ms.B_no_ban, type = "response"))
+
+linear_probability_model <- c(lm.3.2$coefficients[2],
+                              coeftest(lm.3.2, vcov.=vcovHC(lm.3.2, "HC1"))[2,2],
+                              NA,
+                              predict(lm.3.2, Mr.A, type = "response"),
+                              predict(lm.3.2, Mr.A_no_ban, type = "response"),
+                              predict(lm.3.2, Mr.A, type = "response") - predict(lm.3.2, Mr.A_no_ban, type = "response"),
+                              NA,
+                              predict(lm.3.2, Ms.B, type = "response"),
+                              predict(lm.3.2, Ms.B_no_ban, type = "response"),
+                              predict(lm.3.2, Ms.B, type = "response") - predict(lm.3.2, Ms.B_no_ban, type = "response"))
+
+my_table <- tibble(rows = rows, probit_model, linear_probability_model) %>%
+  mutate(probit_model = round(probit_model, 2),
+         linear_probability_model = round(linear_probability_model, 2)) %>%
+  mutate(probit_model = as.character(probit_model),
+         linear_probability_model = as.character(linear_probability_model)) %>%
+  replace_na(list(probit_model = '', linear_probability_model = '')) %>%
+  mutate(probit_model = case_when(rows == '(standard errors in parentheses)' ~  paste('(', probit_model, ')'), 
+                                  .default = probit_model),
+         linear_probability_model = case_when(rows == '(standard errors in parentheses)' ~  paste('(', linear_probability_model, ')'),
+                                              .default = linear_probability_model))
